3dcadf61e539bf11ff5d5fff0c4baa24ab6126d0
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-12-03 15:28:18 +0100

    add the option to have an external installation for the logstash installation
    Fixes #1

--- suite.rb
+++ suite.rb
@@ -7,16 +7,17 @@ BASE_DIR = File.expand_path(File.dirname(__FILE__))
 
 ## script main
 
-if ARGV.size != 1
-  $stderr.puts("usage: ruby suite.rb [suite file]")
+if ARGV.size < 1 or ARGV.size > 2
+  $stderr.puts("usage: ruby suite.rb [suite file] [logstash path]")
   exit(1)
 end
 
 @debug = !!ENV["DEBUG"]
 
-tests = eval(IO.read(ARGV[0]))
-lines = ["name, #{Runner.headers.join(',')}"]
-first = true
+install_path = ARGV.size > 1 ? ARGV[1] : 'logstash'
+tests        = eval(IO.read(ARGV[0]))
+lines        = ["name, #{Runner.headers.join(',')}"]
+first        = true
 
 reporter = Thread.new do
   loop do
@@ -32,7 +33,7 @@ tests.each do |test|
   config = File.join(BASE_DIR, test[:config])
   input  = File.join(BASE_DIR, test[:input])
 
-  runner = Runner.new(config, @debug)
+  runner = Runner.new(config, @debug, install_path)
   p, elaspsed, events_count = runner.run(events, time, runner.read_input_file(input))
 
   lines << "#{test[:name]}, #{"%.2f" % elaspsed}, #{events_count}, #{"%.0f" % (events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}"
