0ac2c7300a75d4b878075fe3f44469e21a2d2707
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-12-08 11:37:56 +0100

    add basic testing for suites

---
+++ spec/fixtures/basic_suite.rb
@@ -0,0 +1,4 @@
+[
+  {:name => "simple 1", :config => "spec/fixtures/simple.conf", :input => "spec/fixtures/simple_10.txt", :time => 5},
+  {:name => "simple 2", :config => "spec/fixtures/simple.conf", :input => "spec/fixtures/simple_10.txt", :time => 10},
+]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $LOAD_PATH.unshift File.join(ROOT, 'lib')
+$LOAD_PATH.unshift File.join(ROOT, 'spec')
 
 require 'lsit'
 
